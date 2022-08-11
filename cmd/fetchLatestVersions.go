24a88d98a0b8c23c4f767c2abe8976f946362376
Jens Schneider <schneider@23technologies.cloud>
2022-08-11 15:15:17 +0200

    Add fetchLatestVersions command
    Further update config.yaml to latest versions. Note that this has no
    impact on the update function, as the version field in config.yaml is
    ignored in this function.
    
    Signed-off-by: Jens Schneider <schneider@23technologies.cloud>

---
+++ cmd/fetchLatestVersions.go
@@ -0,0 +1,51 @@
+package cmd
+
+import (
+	"context"
+	"strings"
+
+	"github.com/gardener-community/gardener-chart-releaser/pkg/releaser"
+	"github.com/google/go-github/v36/github"
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+	"golang.org/x/oauth2"
+)
+
+// fetchLatestVersionsCmd represents the fetchLatestVersions command
+var fetchLatestVersionsCmd = &cobra.Command{
+	Use:   "fetchLatestVersions",
+	Short: "Fetches the latest versions from upstream and writes in the config file",
+	Long: `This is a utility command for updating all versions specified in config.yaml.
+It comes handy, when charts are exported for development purposes and one wants to
+export the most recent version.
+`,
+	Run: func(cmd *cobra.Command, args []string) {
+
+		config := releaser.Configuration{}
+		viper.Unmarshal(&config)
+		ghToken := viper.GetString("GITHUB_TOKEN")
+
+		// get a *github.Client	for the github token
+		// this client will be used for interacting with the github api
+		ts := oauth2.StaticTokenSource(
+			&oauth2.Token{AccessToken: ghToken},
+		)
+		tokenClient := oauth2.NewClient(context.Background(), ts)
+		client := github.NewClient(tokenClient)
+
+		// main loop over all items in the config file
+		for i, cfg := range config.SrcCfg {
+			owner := strings.Split(cfg.Repo, "/")[0]
+			repo := strings.Split(cfg.Repo, "/")[1]
+			latestRelease, _, _ := client.Repositories.GetLatestRelease(context.Background(), owner, repo)
+			config.SrcCfg[i].Version = *latestRelease.TagName
+		}
+		viper.Set("sources", config.SrcCfg)
+		viper.WriteConfig()
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(fetchLatestVersionsCmd)
+
+}
